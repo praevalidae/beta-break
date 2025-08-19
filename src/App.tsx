2ac82a501d2a5e9bdbc5d1050378d5dd3d5563cf
Chris Tran <christopher@luminarycloud.com>
2025-08-19 15:58:46 -0400

    Add background and borders

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { getRandomBetaCard, getRandomBreakCard, getRandomEliminateCard } from './helpers';
 import { styled } from '@mui/system';
 import CardBack from './cards/back.jpg';
-import styling from "App.css"
+import Gym from "./cards/gym.jpg"
+import './App.css';
+import { Dialog } from '@mui/material';
 
 const MainContainer = styled('div')({
-  backgroundColor: '#aaa',
+  backgroundImage: `url(${Gym})`,
+  backgroundSize: 'cover',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'center',
@@ -21,7 +24,6 @@ const CardContainer = styled('div')({
   justifyContent: 'space-between',
   height: '100%',
   width: '100%',
-  gap: '1rem',
 });
 
 const StyledImage = styled('img')({
@@ -35,37 +37,122 @@ const StyledImageWrapper = styled('div')({
   padding: '1rem',
 });
 
+const DialogImageWrapper = styled('div')({
+  padding: '1rem',
+  maxHeight: '100vh',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+const DialogImage = styled('img')({
+  maxWidth: '100%',
+  maxHeight: 'calc(100vh - 2rem)', // Account for padding
+  objectFit: 'contain',
+  borderRadius: '1rem',
+});
+
 function App() {
-  const [drawnCards, setDrawnCards] = useState<string[]>([]);
-  const [currentBetaCard, setCurrentBetaCard] = useState<string>(getRandomBetaCard());
-  const [currentBreakCard, setCurrentBreakCard] = useState<string>(getRandomBreakCard());
+  const [currentBetaCard, setCurrentBetaCard] = useState<string>('');
+  const [currentBreakCard, setCurrentBreakCard] = useState<string>('');
+  const [currentDialogCard, setCurrentDialogCard] = useState<string>('');
+  const [isDialogOpen, setIsDialogOpen] = useState<boolean>(false);
+
+  useEffect(() => {
+    setCurrentBetaCard(getRandomBetaCard());
+    setCurrentBreakCard(getRandomBreakCard());
+  }, []);
 
   const handleDrawCard = () => {
     const randomIndex = Math.floor(Math.random() * 3);
 
-    // for a drawn card
-    // if it's an existing beta or break card, draw another one
+    if (randomIndex === 0) {
+      // set a beta card
+      let randomBetaCard = getRandomBetaCard();
 
-    // show the dialog with the card
-    // if it's an eliminate, just update the eliminate count
-    // if it's a valid beta break card, update the current beta and break cards
+      while (currentBetaCard === randomBetaCard) {
+        randomBetaCard = getRandomBetaCard();
+      }
+
+      setCurrentBetaCard(randomBetaCard);
+      setCurrentDialogCard(randomBetaCard);
+      setIsDialogOpen(true);
+      return;
+    }
+
+    if (randomIndex === 1) {
+      let randomBreakCard = getRandomBreakCard();
+
+      while (currentBreakCard === randomBreakCard) {
+        randomBreakCard = getRandomBreakCard();
+      }
+
+      setCurrentBreakCard(randomBreakCard);
+      setCurrentDialogCard(randomBreakCard);
+      setIsDialogOpen(true);
+      return;
+    }
+
+    if (randomIndex === 2) {
+      setCurrentDialogCard(getRandomEliminateCard());
+      setIsDialogOpen(true);
+      return;
+    }
   };
 
   return (
     <MainContainer>
       <CardContainer>
         <StyledImageWrapper>
-          <StyledImage src={currentBetaCard} alt="Beta" />
+          <div className="shimmer-border">
+            <div className="shimmer-content">
+              <StyledImage src={currentBetaCard} alt="Beta" />
+            </div>
+          </div>
         </StyledImageWrapper>
 
         <StyledImageWrapper>
-          <StyledImage src={currentBreakCard} alt="Break" />
+          <div className="shimmer-border">
+            <div className="shimmer-content">
+              <StyledImage src={currentBreakCard} alt="Break" />
+            </div>
+          </div>
         </StyledImageWrapper>
 
-        <StyledImageWrapper>
-          <StyledImage src={CardBack} alt="deck" />
+        <StyledImageWrapper onClick={handleDrawCard}>
+          <div className="shimmer-border">
+            <div className="shimmer-content">
+              <StyledImage src={CardBack} alt="deck" />
+            </div>
+          </div>
         </StyledImageWrapper>
       </CardContainer>
+
+      <Dialog
+        sx={{
+          backgroundColor: 'transparent',
+          '& .MuiDialog-paper': {
+            maxHeight: '100vh',
+            maxWidth: '100vw',
+            margin: 0,
+            backgroundColor: 'transparent',
+            boxShadow: 'none',
+            overflow: 'hidden',
+          },
+        }}
+        maxWidth={false}
+        open={isDialogOpen}
+        onClick={() => setIsDialogOpen(false)}
+        onClose={() => setIsDialogOpen(false)}
+      >
+        <DialogImageWrapper>
+          <div className="shimmer-border">
+            <div className="shimmer-content">
+              <DialogImage src={currentDialogCard} alt="Dialog" />
+            </div>
+          </div>
+        </DialogImageWrapper>
+      </Dialog>
     </MainContainer>
   );
 }
